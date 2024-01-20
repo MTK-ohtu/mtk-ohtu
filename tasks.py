6301204f63cbba9791efc30e4b196e7ed6feb90b
jhakkari <juuso.hakkari@helsinki.fi>
2024-01-20 15:22:33 +0200

    Update pylint rules

--- tasks.py
+++ tasks.py
@@ -8,3 +8,7 @@ def start(ctx):
 def test(ctx):
     ctx.run("coverage run --branch -m pytest src", pty=True)
     ctx.run("coverage html", pty=True)
+
+@task
+def lint(ctx):
+    ctx.run("pylint src", pty=True)
