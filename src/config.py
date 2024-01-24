e81457809a94cd1b6f5c687807de9f0c39aa564d
inanimateobject <mirko.vonbell@helsinki.fi>
2024-01-24 21:14:13 +0200

    database connection beginings

--- src/config.py
+++ src/config.py
@@ -1,6 +1,10 @@
 from os import getenv
 
 SECRET_KEY = getenv("SECRET_KEY")
+DATABASE_URL= getenv("DATABASE_URL")
+DATABASE_NAME= getenv("DATABASE_NAME")
+DATABASE_USER= getenv("DATABASE_USER")
+DATABASE_PASSWORD= getenv("DATABASE_PASSWORD")
 
 if SECRET_KEY in (None, "PUT_THE_KEY_HERE"):
     raise Exception("Missing SECRET_KEY enviroment variable.")
