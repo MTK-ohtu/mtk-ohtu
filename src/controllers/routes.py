01ecac81688c67260b7fdda04c6185853486e116
Ahannila <arttuhannila123@gmail.com>
2024-01-25 16:07:44 +0200

    added start and end locations

--- src/controllers/routes.py
+++ src/controllers/routes.py
@@ -24,12 +24,14 @@ def login():
 @controller.route("/distance", methods=["get", "post"])
 def distance():
     if request.method == "GET":
-        return render_template("distance.html")
+        return render_template("distance.html", start_location = (0,0), end_location=(0,0))
     if request.method == "POST":
         address1 = request.form["address1"]
         address2 = request.form["address2"]
         qt = int(request.form["quantity"])
         route = route_calculator.Route(address1, address2, "miko.paajanen@helsinki.fi")
+        start_location = (route.location1.latitude, route.location1.longitude)
+        end_location = (route.location2.latitude, route.location2.longitude)
 
         return render_template(
             "distance.html",
@@ -37,5 +39,7 @@ def distance():
             #"duration=str(datetime.timedelta(seconds=(route.duration))).split(".")[0],
             duration=str(datetime.timedelta(seconds=(round(route.duration)))),
             geodesic_distance=round(route.geodesic_distance()/1000,1),
-            price = round(route.distance/1000,1)*qt*0.5
+            price = round(route.distance/1000,1)*qt*0.5,
+            start_location = start_location,
+            end_location = end_location
         )
