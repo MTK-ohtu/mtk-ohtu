786470dc5c3a223ba9ab9388b9414661ee8885c6
KyperCT <38316792+KyperCT@users.noreply.github.com>
2024-02-01 11:52:20 +0200

    Reactivate db cmds

--- src/app.py
+++ src/app.py
@@ -8,8 +8,8 @@ from config import DATABASE_CONFIG
 def create_app():
     app = Flask(__name__)
     # app.secret_key = SECRET_KEY
-    # db_excecute_file("schema.sql", DATABASE_CONFIG)
-    # db_excecute_file("db_mock_data.sql", DATABASE_CONFIG)
+    db_excecute_file("schema.sql", DATABASE_CONFIG)
+    db_excecute_file("db_mock_data.sql", DATABASE_CONFIG)
     app.register_blueprint(controller)  
 
     return app
