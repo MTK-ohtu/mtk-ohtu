c4699fb8a462a9ff58b01894fb50f027f2eff5e2
KyperCT <38316792+KyperCT@users.noreply.github.com>
2024-02-05 12:43:09 +0200

    updated to new schema

--- schema.sql
+++ schema.sql
@@ -4,39 +4,74 @@ CREATE TABLE IF NOT EXISTS users (
     password TEXT NOT NULL,
     email TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
-    roles TEXT[]
+    company_role_id TEXT
 );
 
-CREATE TABLE IF NOT EXISTS sellers (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(id),
-    name TEXT
+CREATE TABLE IF NOT EXISTS companies {
+    id INTEGER SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES users(id),
+    name TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    business_id VARCHAR(16) NOT NULL,
+    standard_industrial_classification VARCHAR(16) NOT NULL
+};
+
+CREATE TYPE buying_or_selling AS ENUM ('sell','buy');
+CREATE TYPE delivery_method_type  AS ENUM ('pickup', 'seller delivers', 'freight');
+CREATE TYPE supply_demand_type AS ENUM ('one time', 'recurring', 'annually', 'weekly');
+CREATE TYPE batch_units_type AS ENUM ('tn', 'm3', 'kg', 'l', 'pcs', 'batch');
+CREATE TYPE vehichle_requirement_type AS ENUM ('dry', 'refrigerated', 'tanker', 'flatbed', 'container');
+CREATE TYPE category_type AS ENUM (
+    'Manure',
+    'Grass, waste fodder and green growths',
+    'Basket fodder',
+    'Plant-based biomasses',
+    'Animal-based biomasses',
+    'Soil and growing media',
+    'Digestion',
+    'Wood',
+    'Other side streams (not biomass)',
+    'Logistics and contracting',
+    'Other'
 );
 
-CREATE TABLE IF NOT EXISTS products (
-    id SERIAL PRIMARY KEY,
-    seller_id INTEGER REFERENCES sellers(id),
-    name TEXT,
-    price INTEGER,
+CREATE TABLE IF NOT EXISTS listings {
+    id INTEGER SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES users(id),
+    listing_type buying_or_selling NOT NULL,
+    category category_type NOT NULL,
+    subcategory VARCHAR(16),
+    delivery_method delivery_method_type NOT NULL,
+    supply_demand supply_demand NOT NULL,
+    is_continuous BOOLEAN DEFAULT 1,
+    expiration_date TIMESTAMP,
+    batch_size INTEGER NOT NULL,
+    batch_units batch_units_type NOT NULL,
+    image_id INTEGER,
+    price NUMERIC NOT NULL,
+    delivery_details TEXT,
     description TEXT,
-    image TEXT,
-    created_at TIMESTAMP DEFAULT NOW(),
-    location TEXT
-);
+    address TEXT NOT NULL,
+    coordinates GEOMETRY(POINT, 4326),
+    vehichle_requirement vehichle_requirement_type,
+    complies_with_regulations BOOLEAN DEFAULT 0
+};
 
-CREATE TABLE IF NOT EXISTS orders (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(id),
-    product_id INTEGER REFERENCES products(id),
-    quantity INTEGER,
+CREATE TABLE IF NOT EXISTS purchases {
+    id INTEGER SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES users(id),
+    listing_id INTEGER NOT NULL REFERENCES listings(id),
+    quantity INTEGER NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
-    status TEXT
-);
+    start_address TEXT NOT NULL,
+    delivery_address TEXT NOT NULL
+};
 
-CREATE TABLE IF NOT EXISTS buyers (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(id),
-    name TEXT,
-    address TEXT,
-    phone TEXT
-);
+CREATE TABLE IF NOT EXISTS logistics_contractors {
+    id INTEGER SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    business_id VARCHAR(16),
+    address TEXT NOT NULL,
+    cargo_capabilities vehichle_requirement_type[]
+};
