fdc4c3783a7eea6fd2e0e1bdc4f8ecf1ea18e66d
Ahannila <arttuhannila123@gmail.com>
2024-02-12 16:50:20 +0200

    Added routes for register and logout

--- src/controllers/routes.py
+++ src/controllers/routes.py
@@ -87,7 +87,26 @@ def login():
             return render_template(
                 "login.html", message="Salasana tai käyttäjätunnus väärin"
             )
+        
+@controller.route("/logout")
+def logout():
+    users.logout()
+    return redirect("/")
 
+@controller.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        email = request.form["email"]
+        if users.register(username, password, email):
+            return redirect("/")
+        else:
+            return render_template(
+                "register.html", message="Käyttäjätunnus on jo käytössä"
+            )
 
 @controller.route("/distance", methods=["get", "post"])
 def distance():
