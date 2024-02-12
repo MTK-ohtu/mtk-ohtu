2e93f998d0560bc8bc71bae49a626581b3426e34
KyperCT <38316792+KyperCT@users.noreply.github.com>
2024-02-12 11:44:05 +0200

    fixed pool bug

--- src/database/database.py
+++ src/database/database.py
@@ -1,6 +1,5 @@
 import psycopg
 from psycopg_pool import ConnectionPool
-from database.db_meta import DatabaseConfig, db_connect
 
 # pylint: disable=E1129
 
@@ -10,17 +9,15 @@ def db_get_product_list(pool: ConnectionPool) -> list:
         config: Database config
     Returns: List of tuples, tuples in format ('product name', 'product price', 'product location', 'product description', 'seller name', longitude, latitude)
     """
-    with pool:
-        out = None
-        with pool.connection() as connection:
-            cursor = connection.cursor()
-            cursor.execute(
-                "SELECT l.category, l.price, l.address, l.description, u.username, l.longitude, l.latitude, l.id \
-                            FROM listings as l \
-                            LEFT JOIN users AS u ON u.id = l.user_id;"
-            )
-            out = list(cursor.fetchall())
-    print(type(out[0][0]))
+    out = None
+    with pool.connection() as connection:
+        cursor = connection.cursor()
+        cursor.execute(
+            "SELECT l.category, l.price, l.address, l.description, u.username, l.longitude, l.latitude, l.id \
+                        FROM listings as l \
+                        LEFT JOIN users AS u ON u.id = l.user_id;"
+        )
+        out = list(cursor.fetchall())
     return out
 
 def db_get_product_by_id(product_id: int, pool: ConnectionPool) -> tuple:
@@ -32,16 +29,15 @@ def db_get_product_by_id(product_id: int, pool: ConnectionPool) -> tuple:
     """
     out = None
     with pool.connection() as connection:
-        with connection:
-            cursor = connection.cursor()
-            cursor.execute(
-                "SELECT l.category, l.price, l.address, l.description, u.username, l.longitude, l.latitude \
-                            FROM listings as l \
-                            LEFT JOIN users AS u ON u.id = l.user_id \
-                            WHERE l.id=%s;",
-                (product_id,),
-            )
-            out = cursor.fetchone()
+        cursor = connection.cursor()
+        cursor.execute(
+            "SELECT l.category, l.price, l.address, l.description, u.username, l.longitude, l.latitude \
+                        FROM listings as l \
+                        LEFT JOIN users AS u ON u.id = l.user_id \
+                        WHERE l.id=%s;",
+            (product_id,),
+        )
+        out = cursor.fetchone()
     return out
 
 def db_get_user(username: str, password: str, pool: ConnectionPool) -> bool:
@@ -53,13 +49,12 @@ def db_get_user(username: str, password: str, pool: ConnectionPool) -> bool:
     Returns: True if user exists and password is correct, False otherwise
     """
     out = False
-    with pool.connection as connection:
-        with connection:
-            cursor = connection.cursor()
-            cursor.execute("SELECT id, password FROM users WHERE username=%s;", (username,))
-            user = cursor.fetchone()
-            if user:
-                out = user[1] == password
+    with pool.connection() as connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT id, password FROM users WHERE username=%s;", (username,))
+        user = cursor.fetchone()
+        if user:
+            out = user[1] == password
     return out
 
 
@@ -76,18 +71,17 @@ def db_add_user(
     """
     out = False
     with pool.connection() as connection:
-        with connection:
-            cursor = connection.cursor()
-            try:
-                cursor.execute(
-                    "INSERT INTO users (username, password, email) (%s,%s,%s)",
-                    (username, password, email),
-                )
-            except psycopg.errors.UniqueViolation:
-                return (False, None)
-            cursor.execute("SELECT id FROM users WHERE username=%s;", (username,))
-            user = cursor.fetchone()
-            out = (True, user[0])
+        cursor = connection.cursor()
+        try:
+            cursor.execute(
+                "INSERT INTO users (username, password, email) (%s,%s,%s)",
+                (username, password, email),
+            )
+        except psycopg.errors.UniqueViolation:
+            return (False, None)
+        cursor.execute("SELECT id FROM users WHERE username=%s;", (username,))
+        user = cursor.fetchone()
+        out = (True, user[0])
     return out
 
 
@@ -111,16 +105,15 @@ def db_add_logistics(
     """
     out = False
     with pool.connection() as connection:
-        with connection:
-            cursor = connection.cursor()
-            try:
-                cursor.execute(
-                    "INSERT INTO logistics_contractors (name, business_id, address) VALUES (%s,%s,%s)",
-                    (name, business_id, address)
-                )
-            except psycopg.Error as e:
-                print(f"Error inserting data: {e}")
-            out = True
+        cursor = connection.cursor()
+        try:
+            cursor.execute(
+                "INSERT INTO logistics_contractors (name, business_id, address) VALUES (%s,%s,%s)",
+                (name, business_id, address)
+            )
+        except psycopg.Error as e:
+            print(f"Error inserting data: {e}")
+        out = True
     return out
 
 def db_add_vehicle(vehicle: str, pool: ConnectionPool):
@@ -139,8 +132,7 @@ def db_get_vehicle_categories(pool: ConnectionPool) -> list:
     Returns: Vehicle categories as a list"""
     out = False
     with pool.connection() as connection:    
-        with connection:
-            cursor = connection.cursor()
-            cursor.execute("SELECT unnest(enum_range(NULL::vehichle_requirement_type))")
-            out = [row[0] for row in cursor.fetchall()]
+        cursor = connection.cursor()
+        cursor.execute("SELECT unnest(enum_range(NULL::vehichle_requirement_type))")
+        out = [row[0] for row in cursor.fetchall()]
     return out
