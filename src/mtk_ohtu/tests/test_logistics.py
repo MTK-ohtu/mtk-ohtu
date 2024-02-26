4d73b357d6e8a0e8f999931f5e82dc0f60dda8ae
AapoTuulentie <aapo.tuulentie@gmail.com>
2024-02-26 10:52:36 +0200

    tests fixed

--- src/mtk_ohtu/tests/test_logistics.py
+++ src/mtk_ohtu/tests/test_logistics.py
@@ -10,10 +10,14 @@ class TestLogistics(unittest.TestCase):
         db_m.db_excecute_file("db_mock_data.sql", DATABASE_CONFIG)
         self.pool = db_m.db_connection_pool(DATABASE_CONFIG)
 
-        self.userid = 1
+        self.user_id = 1
+        self.contractor_id = 1
+        self.contractor_location_id = 1
         self.name = "rekka oy"
         self.business = "1234567-1"
         self.address = "Simonkatu 6"
+        self.email = "rekka@gmail.com"
+        self.phone = "040-1234567"
         self.radius = 200
         self.categories = ["Manure"]
         self.base_rates = ["200"]
@@ -21,36 +25,41 @@ class TestLogistics(unittest.TestCase):
         self.max_capacities = ["50"]
         self.max_distances = ["300"]
 
-    def test_addlogistics_returns_false_when_address_not_found(self):
-        address = "aaaaaaaaaaaaaaaaaaaaa"
-        result = l.addlogistics(
-            self.userid,
-            self.name,
-            self.business,
-            address,
-            self.radius,
-            self.categories,
-            self.base_rates,
-            self.prices_per_hour,
-            self.max_capacities,
-            self.max_distances,
-        )
-        self.assertFalse(result)
+    def test_add_contractor(self):
+        result = l.add_contractor(self.user_id, self.name, self.business)
+        self.assertTrue(result)
 
-    def test_addlogistics_returns_true_when_data_is_correct(self):
-        result = l.addlogistics(
-            self.userid,
-            self.name,
-            self.business,
+    def test_add_contractor_location(self):
+        result = l.add_contractor_location(
+            self.contractor_id,
             self.address,
-            self.radius,
-            self.categories,
-            self.base_rates,
-            self.prices_per_hour,
-            self.max_capacities,
-            self.max_distances,
+            self.phone,
+            self.email,
+            self.radius
         )
         self.assertTrue(result)
+
+    def test_add_cargo_capability(self):
+        result = l.add_cargo_capability(
+            self.contractor_location_id,
+            self.categories,
+            self.base_rates,
+            self.prices_per_hour,
+            self.max_capacities,
+            self.max_distances
+        )
+        self.assertTrue(result)
+
+    def test_add_contractor_location_returns_false_when_address_not_found(self):
+        self.address = "aaaaaaaaaaaaaaaaaaaaa"
+        result = l.add_contractor_location(
+            self.contractor_id,
+            self.address,
+            self.phone,
+            self.email,
+            self.radius
+        )
+        self.assertFalse(result)
 
     def tearDown(self):
         self.pool.close()
