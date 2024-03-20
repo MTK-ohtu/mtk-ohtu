b3d0ef816624331ad5906381577b60d42a7e5469
Otso <24369914+0tso@users.noreply.github.com>
2024-03-20 13:52:14 +0200

    404 page added; invalid routes crashing fixed

--- src/mtk_ohtu/routes/misc.py
+++ src/mtk_ohtu/routes/misc.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, render_template, g, redirect
+from flask import Blueprint, request, render_template, g, redirect, abort
 
 misc_bp = Blueprint("misc_bp", __name__)
 
@@ -13,3 +13,11 @@ def change_language(new_language: str):
     p = "/" + "/".join([new_language] + split_path[2:])
     g.lang_code = new_language
     return redirect(p)
+
+# A route that accepts all routes not accepted by others
+# Needed due to languages; otherwise inputting an invalid address will lead to:
+# /X  /en/X  /en/en/X  /en/en/en/X  /en/en/en/en/X  /en/en/en/en/X  etc...
+# because there is no route that accepts it and therefore the language parameter is not caught. There needs to be a route to catch it.
+@misc_bp.route("/<path:p>")
+def fallback_errorhandler(p: str):
+    return render_template("404.html")
